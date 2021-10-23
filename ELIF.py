835326c457e08ea7e360e8af55548153ba8b3c9d
joseadrianoemmanuel1999 <73005217+joseadrianoemmanuel1999@users.noreply.github.com>
2021-10-23 13:12:46 +0100

    Add files via upload

---
+++ ELIF.py
@@ -0,0 +1,19 @@
+name = str(input("Digite o teu nome: "))
+idade = int(input("Digite a tua idade: "))
+
+if name == 'Jose':
+    print('Seja Bem vindo Senhor Jose')
+
+
+elif idade < 12:
+
+    print("Maximus")
+
+elif idade > 2000:
+
+    print(" Vampiro ou undead")
+elif idade < 100:
+    print("Tu n e o Jose, Mas sim Magina")
+
+
+    
